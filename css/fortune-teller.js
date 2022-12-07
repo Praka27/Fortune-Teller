2116e5fbc9d6138f87709937b900952c1000a2a0
Praka27 <prakasithnoinala@yahoo.com.au>
2022-12-08 03:49:57 +1000

    change one field to number

--- css/fortune-teller.js
+++ css/fortune-teller.js
@@ -2,49 +2,49 @@
 const Card = [{
     Sign: 'I see the reaper has come',
     Reading :'In the near future this says an end to something old and a new begining of life ',
-    Result: "The cycle of change begins in 2 days"
+    Result: 2
   },
   { Sign: 'Love is in the air',
     Reading: 'Time for a new love relationship',
-    Result: "The cycle of change begins in 12 days"
+    Result: 12
   },
   { Sign: 'Family coming together',
     Reading: 'A new member added to your family',
-    Result: "The cycle of change begins in 7 days"
+    Result: 7
   },
   { Sign: 'The moon is now full',
     Reading: 'Time of mystic creations come into your life',
-    Result: "The cycle of change begins in 14 days"
+    Result: 14
   },
   { Sign: 'The Sun shines bright',
     Reading: 'Time to put ideas and sow the seeds for the future',
-    Result: "The cycle of change begins in 21 days"
+    Result: 21
   },
   { Sign: 'Storm clouds are ahead',
     Reading: 'A new conflict is approaching in the near future',
-    Result: "The cycle of change begins in 24 days"
+    Result: 24
   },
   { Sign: 'This is the Storm Card',
     Reading: 'A new conflict is approaching in the near future',
-    Result: "The cycle of change begins in 11 days"
+    Result: 11
   },
   { Sign: 'The bearer of children comes near',
     Reading: 'Tragedy foretold for a family member',
-    Result: "The cycle of change begins in 6 days"
+    Result: 6
   },
   { Sign: 'The eagle eye has awaken',
     Reading: 'Time to focus on oneself to sow the seeds of the future',
-    Result: "The cycle of change begins in 28 days"
+    Result: 28
   },
   { Sign: 'The cold ice age has come',
     Reading: 'Hard and difficult times are ahead',
-    Result: "The cycle of change begins in 31 days"
+    Result: 31
   }];
 
 //Randomize between 3 fields in array of objects
 
 const Random_reading = () => {
-  const reading_card = Math.floor(Math.random()*3);
+  const reading_card = Math.floor((Math.random()*3)+1);
   return reading_card;
 }
   
@@ -57,19 +57,16 @@ const Randomcard = () => {
 
 //Declare Random variables and assign to varibale message before passing to Html document id = result
 
-let messagedata = 1;
-let data = 1;
-
 const message = () => {
   let messages = '';
   messagedata = Randomcard();
   data = Random_reading();
-  if (data === 0) {
+  if (data === 1) {
     messages = Card[messagedata].Sign;
-  } else if (data === 1) {
-    messages = Card[messagedata].Reading;
   } else if (data === 2) {
-    messages = Card[messagedata].Result;
+    messages = Card[messagedata].Reading;
+  } else if (data === 3) {
+    messages = "The cycle of change begins in " + String(Card[messagedata].Result) + " days";
   };
   document.getElementById("result").value = messages;
 };
